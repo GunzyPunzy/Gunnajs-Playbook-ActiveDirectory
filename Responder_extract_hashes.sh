917351050b5fd5b847c32e4278def6ea6054ca87
gunnaj <122086158+GunzyPunzy@users.noreply.github.com>
2023-08-21 10:36:29 +0200

    Delete Responder_extract_hashes.sh

--- Responder_extract_hashes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for user in `strings Responder-Session.log | grep "NTLMv2-SSP Hash" | cut -d ":" -f 4-6 | sort -u -f | awk '{$1=$1};1'`
-do
-echo "[*] search for: $user";
-strings Responder-Session.log | grep "NTLMv2-SSP Hash" | grep -i $user | cut -d ":" -f 4-10 |  head -n 1 | awk '{$1=$1};1' >> ntlm-hashes.txt
-done
