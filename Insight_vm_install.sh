776b472480943162dabd299e5cf70f55e2ea502c
gunnaj <122086158+GunzyPunzy@users.noreply.github.com>
2023-10-16 09:01:34 +0200

    Update Insight_vm_install.sh
    removed ufw

--- Insight_vm_install.sh
+++ Insight_vm_install.sh
@@ -3,9 +3,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install openssh-server -y
 sudo apt install screen -y
-sudo ufw allow ssh
-sudo ufw allow https
-sudo ufw allow 3780
 
 wget http://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin -P /tmp/
 sudo chmod +x /tmp/Rapid7Setup-Linux64.bin
